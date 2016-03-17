3268adb404d670f38f6cb230128b11dddeafd3cd
Benjamin Gobeil <benjamin.gobeil@usherbrooke.ca>
2016-03-16 23:24:02 -0400

    Ajout du jeu DameUs
    -Jeu de base avec les classes état de base
    -Vient avec la sln et la lib SFML integrer

---
+++ games/DameUs/DameUs/DameUs/main_game.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "game_state.h"
+
+
+class main_game : public tiny_state
+{
+public:
+	void Initialize(sf::RenderWindow* window);
+	void Update(sf::RenderWindow* window);
+	void Render(sf::RenderWindow* window);
+	void Destroy(sf::RenderWindow* window);
+private:
+};
